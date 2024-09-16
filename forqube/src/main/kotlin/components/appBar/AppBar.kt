6c35489cf98ddfb31b4b8ea109c397239e9f61d3
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-09-16 15:34:54 +0530

    Create app bar

---
+++ forqube/src/main/kotlin/components/appBar/AppBar.kt
@@ -0,0 +1,16 @@
+package components.appBar
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun appBar(modifier: Modifier) {
+
+    Row(
+        modifier = modifier
+    ) {
+
+    }
+}
+
